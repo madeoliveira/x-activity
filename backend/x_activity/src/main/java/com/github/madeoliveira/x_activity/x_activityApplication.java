529b4814ab48147b92c168f6477acef6b7d71215
Your Name <you@example.com>
2022-06-01 16:51:48 -0300

    Configured the swagger

---
+++ backend/x_activity/src/main/java/com/github/madeoliveira/x_activity/x_activityApplication.java
@@ -0,0 +1,13 @@
+package com.github.madeoliveira.x_activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class x_activityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(x_activityApplication.class, args);
+	}
+
+}
